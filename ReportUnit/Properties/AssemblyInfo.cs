7105ab9b7ca2f049f455976d3bcedbac102d5f23
Thach Lockevn <lockevn@gmail.com>
2017-10-24 17:37:11 +0700

    build v1.5.0.1, tweaked by lockevn@

--- ReportUnit/Properties/AssemblyInfo.cs
+++ ReportUnit/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ReportUnit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tweak by Lockevn")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Relevant Codes")]
 [assembly: AssemblyProduct("ReportUnit")]
